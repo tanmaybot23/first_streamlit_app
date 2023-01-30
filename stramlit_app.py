97ee7259a3b0649c8516c9e05de90eacec2583fd
tanmaybot23 <105122233+tanmaybot23@users.noreply.github.com>
2023-01-30 13:30:02 +0530

    Update stramlit_app.py
    added more menus

--- stramlit_app.py
+++ stramlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('hello world!')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
